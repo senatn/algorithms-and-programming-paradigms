a2b107ff2c81e741bc1396ea64773c5e03ebfebd
sena <46344611+senatn@users.noreply.github.com>
2023-05-31 20:04:08 +0300

    modified

--- neetcode-roadmap/arrays-and-hashing/product-of-array-except-self.py
+++ neetcode-roadmap/arrays-and-hashing/product-of-array-except-self.py
@@ -26,3 +26,25 @@ input_list = [int(char) for char in input_list]
 result = solution.productExceptSelf(input_list)
 
 print(result)
+
+
+def product_array(arr):
+    n = len (arr)
+    left, right = [1] * n, [1] * n # we can create 2 arr whre we will store partial product
+    product_array = [] #also initialize an emty array whre we are goung to actually store all of our products once
+    
+    # build left array
+    for i in range(1 ,n): #all we are doing here is basically, for each position in the array we want to take the product of everything that came before it excluding the current position
+        #we are gonna go through the position in the array starting from the first index just because zero with index there is noting before that
+        #we can go ahed and populate each index in our left array
+        left[i] = left[i - 1 ] * arr[i -1] #this is basically saying, we are taking the last position of the left array and multiplying it by the previous number in our input array 
+        
+    # build right array
+    for i in range(1, n):
+        right[i] = right[i-1] * arr[::-1][i-1]
+        
+    # build product array from subarrays
+    for i in range(n):
+        product_array.append(left[i] * right[::-1][i])
+    
+    return product_array
